de81907209f4cfbf6267c09514f034a2690ea4ad
Rebecca <rebeccaplease@gmail.com>
2015-06-30 01:21:09 -0400

    photo library popup option

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
+var photoTakingHelper: PhotoTakingHelper?
+
 class TimelineViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.tabBarController?.delegate = self //since timeline is the first view that is loaded, put delegate in here
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +35,24 @@ class TimelineViewController: UIViewController {
     }
     */
 
+    func takePhoto() {
+        // instantiate photo taking class, provide callback when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in //trailing closure
+            //don't do anything yet
+        }
+    }
+}
+
+//MARK: Tab Bar Delegate
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController (tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false //don't select photo view controller, take photo instead
+        }
+        else {
+            return true // for other view controllers, just select them
+        }
+    }
 }
