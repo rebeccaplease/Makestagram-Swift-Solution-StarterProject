d0ecce4f75d550c758df6f065c074e5dd0942c51
Rebecca <rebeccaplease@gmail.com>
2015-07-02 11:13:29 -0400

    set constants for ParseHelper class

--- Makestagram/Helpers/ParseHelper.swift
+++ Makestagram/Helpers/ParseHelper.swift
@@ -11,28 +11,50 @@ import Parse
 
 class ParseHelper {
     
+    // Following Relation
+    static let ParseFollowClass       = "Follow"
+    static let ParseFollowFromUser    = "fromUser"
+    static let ParseFollowToUser      = "toUser"
+    
+    // Like Relation
+    static let ParseLikeClass         = "Like"
+    static let ParseLikeToPost        = "toPost"
+    static let ParseLikeFromUser      = "fromUser"
+    
+    // Post Relation
+    static let ParsePostUser          = "user"
+    static let ParsePostCreatedAt     = "createdAt"
+    
+    // Flagged Content Relation
+    static let ParseFlaggedContentClass    = "FlaggedContent"
+    static let ParseFlaggedContentFromUser = "fromUser"
+    static let ParseFlaggedContentToPost   = "toPost"
+    
+    // User Relation
+    static let ParseUserUsername      = "username"
+    
     static func timelineRequestForCurrentUser(completionBlock: PFArrayResultBlock) {
         
         //retrieve Follow relationships from current user
-        let followingQuery = PFQuery(className: "Follow")
-        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        let followingQuery = PFQuery(className: ParseFollowClass)
+        followingQuery.whereKey(ParseFollowFromUser, equalTo:PFUser.currentUser()!)
         
         //retrieve Posts from Follow relationships
         let postsFromFollowedUsers = Post.query()
-        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        postsFromFollowedUsers!.whereKey(ParsePostUser, matchesKey: ParseFollowToUser, inQuery: followingQuery)
         
         //retrieve posts from this user
         let postsFromThisUser = Post.query()
-        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        postsFromThisUser!.whereKey(ParsePostUser, equalTo: PFUser.currentUser()!)
         
         //combine queryy of two sets of posts (from user and from following users)
         let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
         
         //retrieve user associated with posts (since Post stores a pointer to its user)
-        query.includeKey("user")
+        query.includeKey(ParsePostUser)
         
         //sort by date
-        query.orderByDescending("createdAt")
+        query.orderByDescending(ParsePostCreatedAt)
         
         //kick off network request
         query.findObjectsInBackgroundWithBlock(completionBlock)
